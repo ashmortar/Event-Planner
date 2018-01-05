275e733a68acec25fbe0d8129f1b242c2428e927
Aaron Ross <ashmortar@gmail.com>
2018-01-05 08:46:24 -0800

    create a passing test that checks if Event is instantiating correctly

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -1,7 +1,14 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EventTest {
 
+    @Test
+    public void Event_instantiatesCorrectly_true() throws Exception {
+        Event testEvent = new Event();
+        assertEquals(true, testEvent instanceof Event);
+    }
 }
